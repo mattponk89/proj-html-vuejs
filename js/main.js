5cb5d592c1f6e46b86b951211bf933070d643033
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-12-11 14:25:51 +0000

    vueJs implemented

--- js/main.js
+++ js/main.js
@@ -3,5 +3,12 @@ Vue.config.devtools = true
 const app = new Vue({
   el: '#root',
   data: {
-    menu:["Home", "About", "Services", "Journal", "Contact"]
+    menu:["Home", "About", "Services", "Journal", "Contact"],
+    chatActive: false
+  },
+  methods: {
+    toggleChat: function () {
+      this.chatActive = !this.chatActive
+    }
   }
+})
